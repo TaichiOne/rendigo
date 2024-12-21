971a5d761f1095195680e972b57d9a1e01ded68b
Max Pinakhin <Max2duo@mail.ru>
2024-12-22 00:37:42 +0300

    model creation

--- RendigoApp/models.py
+++ RendigoApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class ContactInfo(models.Model):
+    title = models.CharField(max_length=200)  # Заголовок страницы
+    description = models.TextField()  # Описание
+    phone = models.CharField(max_length=20)  # Телефон
+    address = models.CharField(max_length=200)  # Адрес
+    email = models.EmailField(max_length=120, blank=True, null=False, default='')  # Почта
+    longitude = models.FloatField()  # Долгота
+    latitude = models.FloatField()  # Широта
+    web_page_url = models.URLField()  # URL веб-страницы
+
+    def __str__(self):
+        return self.title
